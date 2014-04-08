4f22ad47699aa1a44a729acb2f524175cef6f97e
JustinZammit <justin.zammit@mcast.edu.mt>
2014-04-08 14:00:29 +0200

    Implementing Level 1
    I started to implementing Level 1 for the game. Inserted the character
    to test the game

---
+++ GameApplicationAssignment/Assets/Scripts/characterController.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+//var controlling the character's speed
+var characterSpeed:int;
+
+
+
+function Start () {
+		
+}
+
+
+function Move (cSpeed:int) {
+	
+	transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal"));
+
+	if (Input.GetKey(KeyCode.UpArrow))
+	{
+		transform.Translate(Vector3.up * Time.deltaTime * cSpeed);
+	}
+	
+	if (Input.GetKey(KeyCode.DownArrow))
+	{
+		transform.Translate(Vector3.down * Time.deltaTime * cSpeed);
+	}
+
+
+}
+
+function Update () {
+	//transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal"));
+	
+		Move (5);
+}
