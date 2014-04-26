d5d12428c43c6c175b5e24d15b1e6a542d210696
JustinZammit <justin.zammit@mcast.edu.mt>
2014-04-26 20:22:50 +0200

    Doing Animated Sprite Sheet with Arrow buttons
    I have implemented the movement with arrow buttons left right and jump
    while I also done the sprite sheet player animation. On pressing left or
    right, the player will animate itself. Created some java scripts
    accordingly

---
+++ GameApplicationAssignment/Assets/Scripts/PlayerAnimate.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+function Start () {
+
+
+}
+
+function Update () {
+
+var AT = gameObject.GetComponent(AnimatedTexture); //Store AnimatedTexture Script
+
+if (Input.GetKey("right")){//user pushed right arrow key
+	//set sprite to get animated from row 1
+	AT.rowNumber = 1;
+	}
+
+	//set texture to negative position (flip it)
+	else if(Input.GetKey("left")){//user pushed left arrow key
+	//set sprite to get animated from row 1
+	AT.rowNumber = 1;
+	}
+	
+	//set sprite to get animated from row 0 standstill
+	else {
+	AT.rowNumber = 0;
+	}
+}
---
+++ GameApplicationAssignment/Assets/Scripts/TextureFlip.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var X : float;
+
+
+//gathering normal object scale x
+function Start () 
+{
+	X = transform.localScale.x;
+}
+
+
+function Update () 
+{
+	if (Input.GetKey("right")){//user pushed right arrow key
+	//set texture to normal position
+	transform.localScale.x=X;
+	}
+
+	//set texture to negative position (flip it)
+	else if(Input.GetKey("left")){//user pushed left arrow key
+	transform.localScale.x=-X;
+	}
+}
