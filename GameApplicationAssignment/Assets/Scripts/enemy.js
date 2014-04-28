c98bf74f6411f0f470bf89e17b1ea22f4d526f67
JustinZammit <justin.zammit@mcast.edu.mt>
2014-04-28 16:31:15 +0200

    Camera Movement and Player Re-Spawn
    I have created various script for the  camera movement and player
    re-spawn. When the player hits an enemy, the player will re-spawn to its
    starting position again by creating a prefab. I also attached the camera
    to the first person player controller inorder to follow the player on
    play

---
+++ GameApplicationAssignment/Assets/Scripts/enemy.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+private var fall : boolean;
+var Player : GameObject;
+var spawnPoint : Transform;
+var stomp : boolean;
+
+
+function Update () {
+
+}
+
+
+function OnTriggerEnter (other: Collider) {
+	if (!stomp){
+		
+		if(other.tag == "Player"){
+		Destroy(other.gameObject);
+		var P : GameObject = Instantiate(Player, spawnPoint.position, Quaternion.identity);
+
+		}
+	}
+	
+
+}
